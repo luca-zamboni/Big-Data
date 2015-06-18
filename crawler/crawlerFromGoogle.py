b2648def9279fcc243bb21f1540a4239f847161c
Luca <lucazamboni92@gmail.com>
2015-06-19 00:19:07 +0200

    google news crawler

--- crawler/crawlerFromGoogle.py
+++ crawler/crawlerFromGoogle.py
@@ -16,7 +16,7 @@ def remove_tags(raw_html):
 
 def addToFile(testata,title,testo):
 	print("Added news from " + testata)
-	with open("news.txt", "a") as myfile:
+	with open("newsG.txt", "a") as myfile:
 		myfile.write(testata + "\n")
 		myfile.write(title + "\n")
 		myfile.write(testo + "\n")
@@ -33,7 +33,7 @@ def insert(testata,news):
 			testo = remove_tags(testo).strip().rstrip('\n')
 
 			
-			newsFile = open("news.txt", "r")
+			newsFile = open("newsG.txt", "r")
 			tempnews = []
 			while True:
 				testataF = newsFile.readline().rstrip('\n')
@@ -54,21 +54,39 @@ def insert(testata,news):
 
 while(True):
 	try:
-		feed = open("feed.txt", "r")
 
-		for url in feed:
-			url = url.split()
-			response = urllib.request.urlopen(url[1])
+		feed = []
+
+		response = urllib.request.urlopen("https://news.google.it")
+		data = response.read()
+		text = data.decode('utf-8')
+
+		aTags = re.findall('<a [^>.]*>Copertura live</a>', text)
+
+		xmls = []
+		for a in aTags:
+			links = re.findall('href="[^".]*"', a)
+			url = "https://news.google.it/" + links[0][7:-1]
+			response = urllib.request.urlopen(url)
 			data = response.read()
 			text = data.decode('utf-8')
-			m = hashlib.sha1(text.encode()).hexdigest()
 
-			if(url[0] not in md5 != m or md5[url[0]]):
+			hrefRSS = re.findall('<a .*>.*RSS<\/a>', text)
+			tmp = re.findall('href="([^"]*)"', hrefRSS[0])[0].replace("amp;", "");
+			RSS = "https://" + tmp[7:]
 
-				root = ET.fromstring(text)
-				insert(url[0],root.iter('item'))
+			#print(RSS)
+			
+			response = urllib.request.urlopen(RSS)
+			data = response.read()
+			text = data.decode('utf-8')
 
-		feed.close()
+
+			#print(text)
+
+			root = ET.fromstring(text)
+			insert("Google",root.iter('item'))
+
 	except Exception: 
   		print("Except")
 	time.sleep( 5 )
