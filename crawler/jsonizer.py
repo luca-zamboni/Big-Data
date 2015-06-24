86263db53aace66ea3fa20300c52e17d11a20479
Roberto Zen <rob.zenn92@hotmail.it>
2015-06-24 17:33:18 +0200

    jsonizer.py updated

--- crawler/jsonizer.py
+++ crawler/jsonizer.py
@@ -2,7 +2,8 @@ import json
 import re
 import os.path	# files management and checks
 from html.parser import HTMLParser
-from html.entities import name2codepoint # for html cha
+from html.entities import name2codepoint # for html character
+from html5print import HTMLBeautifier
 
 GOOGLE_NEWS_PATH = "newsG.txt"
 
@@ -45,7 +46,6 @@ class MyHTMLParser(HTMLParser):
 
 		# TESTATA
 		if self.current_tag == "font":
-			print("è un font! ",data,self.count_font )
 			if self.count_font == 5:
 				self.news.set_testata(data)	
 			elif self.count_font == 6:
@@ -141,20 +141,63 @@ def parse_news(url, title, source):
 	parser = MyHTMLParser(news)
 	print(news.to_JSON())
 
-def parse_news_file():
+def parse_news_file(path):
 
-	if not os.path.exists(GOOGLE_NEWS_PATH):
-		print("Sorry, no news to parse.")
+	if not os.path.exists(path):
+		print("Sorry, no news to parse in ", path , ".")
 	else:
 
-		newsFile = open(GOOGLE_NEWS_PATH, "r")
+		newsFile = open(path, "r")
 
-		url = newsFile.readline()
-		title = newsFile.readline().rstrip('\n')
-		source = newsFile.readline().rstrip('\n')
+		while True:
 
-		parse_news(url,title,source)
+			url = newsFile.readline().rstrip('\n')
+			title = newsFile.readline().rstrip('\n')
+			source = newsFile.readline().rstrip('\n')
+			
+			# Check emptyness
+			if not url or not title or not source: break
+
+			parse_news(url,title,source)
+
 
 		newsFile.close();
 
-parse_news_file()
+def create_news_files(path):
+
+	if not os.path.exists(path):
+		print("Sorry, no news to parse in ", path , ".")
+	else:
+		strin = '<table border="0" cellpadding="2" cellspacing="7" style="vertical-align:top;"><tr><td width="80" align="center" valign="top"><font style="font-size:85%;font-family:arial,sans-serif"><a href="http://news.google.com/news/url?sa=t&amp;fd=R&amp;ct2=it&amp;usg=AFQjCNGLiCpLOfwJxLlRMrIfOyCLCtI02g&amp;clid=c3a7d30bb8a4878e06b80cf16b898331&amp;cid=52779454240467&amp;ei=ZHqFVYjcGNWoaseAg5gJ&amp;url=http://www.quotidiano.net/austria-suv-contro-passanti-1.1076583"><img src="//t3.gstatic.com/images?q=tbn:ANd9GcRk0XvPCFQ3WrTqXsZhr5JsdJQF0FgPMD4a9SAgfQyDZAnDWfclUEaO_cTMeQIMevsniSBlSAs" alt="" border="1" width="80" height="80"><br><font size="-2">Quotidiano.net</font></a></font></td><td valign="top" class="j"><font style="font-size:85%;font-family:arial,sans-serif"><br><div style="padding-top:0.8em;"><img alt="" height="1" width="1"></div><div class="lh"><a href="http://news.google.com/news/url?sa=t&amp;fd=R&amp;ct2=it&amp;usg=AFQjCNGLiCpLOfwJxLlRMrIfOyCLCtI02g&amp;clid=c3a7d30bb8a4878e06b80cf16b898331&amp;cid=52779454240467&amp;ei=ZHqFVYjcGNWoaseAg5gJ&amp;url=http://www.quotidiano.net/austria-suv-contro-passanti-1.1076583"><b>Austria, con il suv contro la folla: 3 morti Polizia: &quot;Armato, ma non è <b>...</b></b></a><br><font size="-1"><b><font color="#6f6f6f">Quotidiano.net</font></b></font><br><font size="-1">L&#39;autore del folle gesto è un 26enne austriaco di origini bosniache.Il giovane soffre di problemi psichici. Almeno 34 i feriti. Tra le vittime un bimbo di 7 anni e una donna. Austria, uomo si lancia con il suv contro la folla. (Ansa)&nbsp;...</font><br><font size="-1" class="p"></font><br><font class="p" size="-1"><a class="p" href="http://news.google.it/news/story?ncl=dkdCZaFrYIPByEM&amp;ned=it"><nobr><b>altro&nbsp;&raquo;</b></nobr></a></font></div></font></td></tr></table>'
+		f = open("news/gen_0.html", "a+")
+		f.write(HTMLBeautifier.beautify(strin, 4))
+		f.close()
+
+
+		# newsFile = open(path, "r")
+		# count = 0
+
+		# while True:
+
+		# 	count += 1
+
+		# 	url = newsFile.readline().rstrip('\n')
+		# 	title = newsFile.readline().rstrip('\n')
+		# 	source = newsFile.readline().rstrip('\n')
+			
+		# 	# Check emptyness
+		# 	if not url or not title or not source: break
+
+		# 	path_single = "news/gen_" + str(count) + ".html"
+		# 	f = open(path_single, "a+")
+		# 	f.write(HTMLBeautifier.beautify(source, 4))
+		# 	f.close()
+
+		# newsFile.close()
+
+
+create_news_files(GOOGLE_NEWS_PATH)
+# parse_news_file(GOOGLE_NEWS_PATH)
+
+
+
